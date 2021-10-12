f26315cb79a3e594928e394303cb96a38a7dd711
Anthony Kupecz <anthonykupeczbitch@anthonys-mbp-3.myfiosgateway.com>
2021-10-12 13:30:02 -0400

    Im on the interwebs

---
+++ trump.js
@@ -0,0 +1,24 @@
+chrome.runtime.onMessage.addListener(getMessage)
+
+function getMessage(message, sender, sendResponse) {
+    var nicknames = ["Chancellor Cheeto", "Pussy Predator", "Douche L'Orange", "Donnie Chump", "Oompa Loompa", "T###p", "Agent Orange", "The Annoying Orange", "The Fanta Menace"];
+    var page = document.body.innerHTML;
+    var countTrumps = (page.match(/trump/gi) || []).length;
+
+    //Change all the instances of Trump 
+    for (var i = 0; i < countTrumps; i++) {
+        console.log(i);
+        index = Math.floor(Math.random() * nicknames.length);
+        name = nicknames[index];
+        page = page.replace(/trump/i, name);
+    }
+
+    //Reset the page and report
+    document.body.innerHTML = page;
+    
+    sendReport(countTrumps)
+}
+
+function sendReport(num) {
+    alert(String(num) + " Oranges Rotted Away");
+}
